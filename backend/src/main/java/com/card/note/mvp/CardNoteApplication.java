0fb5d5e6a5de55bdc5b99532ddb53509ffd89e67
Alistair10123 <shiearim@gmail.com>
2023-02-27 13:59:12 +0800

    add backend

---
+++ backend/src/main/java/com/card/note/mvp/CardNoteApplication.java
@@ -0,0 +1,13 @@
+package com.card.note.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardNoteApplication.class, args);
+	}
+
+}
